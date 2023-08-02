97bfde22aed494231cb85dbaf33081f44d65bb9b
YuriiKniaz <yuri.kniaz@gmail.com>
2023-08-02 20:17:48 +0300

    Task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+const buttonsStyle = document.querySelector('button');
+let timerId = null;
+
+
+stopBtn.setAttribute('disabled', true);
+
+function btnStart() {
+    
+    stopBtn.removeAttribute('disabled');
+    startBtn.setAttribute('disabled', true);
+    timerId = setInterval(() => { body.style.background = `${getRandomHexColor()}` }, 1000);
+    
+}
+
+startBtn.addEventListener('click', btnStart);
+
+function btnStop() {
+    startBtn.removeAttribute('disabled');
+    stopBtn.setAttribute('disabled', true);
+
+    clearInterval(timerId);
+}
+
+stopBtn.addEventListener('click', btnStop);
+
 
---
+++ src/js/02-timer.js
@@ -0,0 +1,9 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+
+
+
+const second = 1000;
+const minute = second * 60;
+const hour = minute * 60;
+const day = hour * 24;
