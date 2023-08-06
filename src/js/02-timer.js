362e15515463a2dd557f716d8b518358a6dbc810
YuriiKniaz <yuri.kniaz@gmail.com>
2023-08-06 14:13:00 +0300

    Task 2-3

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,9 +1,84 @@
-import flatpickr from "flatpickr";
-import "flatpickr/dist/flatpickr.min.css";
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
 
 
+const daysSpan = document.querySelector('.value[data-days]');
+const hoursSpan = document.querySelector('.value[data-hours]');
+const minutesSpan = document.querySelector('.value[data-minutes]');
+const secondsSpan = document.querySelector('.value[data-seconds]');
+const startBtn = document.querySelector('button[data-start]');
+const dataPicker = document.getElementById('datetime-picker');
 
-const second = 1000;
-const minute = second * 60;
-const hour = minute * 60;
-const day = hour * 24;
+let timerSet = false;
+startBtn.setAttribute('disabled', true);
+
+function addLeadingZero(value) {
+  return value.toString().padStart(2, '0');
+}
+
+const calendar = flatpickr(dataPicker, {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const dateDiff = selectedDates[0].getTime() - new Date().getTime();
+    if (dateDiff > 0 && !timerSet) {
+      startBtn.removeAttribute('disabled');
+
+      startBtn.addEventListener('click', () => {
+        startTimer(dateDiff);
+        timerSet = true;
+        startBtn.setAttribute('disabled', true);
+        Notiflix.Notify.success('Your timer is started')
+      });
+    } else {
+      startBtn.disabled = true;
+      if (timerSet) {
+        Notiflix.Notify.failure('Timer is already seted, refresh page to set new timer');
+      } else {
+        Notiflix.Notify.failure('Please choose the future time');
+      }
+    }
+  },
+});
+
+function startTimer(ms) {
+  let timerId = setInterval(() => {
+    if (ms > 0) {
+      timeSpan(convertMs(ms));
+      ms -= 1000;
+    } else {
+      clearInterval(timerId);
+    }
+  }, 1000);
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function timeSpan(t) {
+  daysSpan.textContent = addLeadingZero(t.days);
+  hoursSpan.textContent = addLeadingZero(t.hours);
+  minutesSpan.textContent = addLeadingZero(t.minutes);
+  secondsSpan.textContent = addLeadingZero(t.seconds);
+  
+    
+}
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,52 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from "notiflix";
+
+const form = document.querySelector('.form');
+const delayInput = document.querySelector('input[name=delay]');
+const stepInput = document.querySelector('input[name=step]');
+const amountInput = document.querySelector('input[name=amount]');
+
+
+
+function onSubmit(e) {
+  e.preventDefault();
+
+  let delay = Number(delayInput.value);
+  let step = Number(stepInput.value);
+  let amount = Number(amountInput.value);
+  let position = 0;
+
+  for (let i = 1; 1 <= amount; i += 1){
+    position = i;
+    createPromise(i, delay)
+  .then(({ position, delay }) => {
+    Notiflix.Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    Notiflix.Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+  });
+    delay += step;
   }
+  form.reset();
 }
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        // Fulfill
+        resolve({ position, delay });
+
+      } else {
+        // Reject
+        reject({ position, delay });
+      }
+    }, delay);
+ 
+  });
+  
+};
+
+form.addEventListener('submit', onSubmit);
+
+
